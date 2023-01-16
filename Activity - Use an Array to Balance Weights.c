775736863993a972e5b68e5568a1b4a3a4b88fea
Brandon <65471994+BearMSU@users.noreply.github.com>
2023-01-16 18:32:58 -0500

    Create Activity - Use an Array to Balance Weights.c

---
+++ Activity - Use an Array to Balance Weights.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void){
+    double cars[50];
+    double tWeight = 0;
+    int nCars;
+    scanf("%d", &nCars);
+    int i;
+    for(i=0; i<nCars; i++){
+        scanf("%lf", &cars[i]);
+        tWeight += cars[i];
+    }
+    double aWeight = tWeight / nCars;
+    for(i=0; i<nCars; i++){
+        printf("%.1lf\n", aWeight - cars[i]);
+    }
+    return 0;
